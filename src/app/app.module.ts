7b83b31e4378cdfe49b897a779fdbcd99c587dc0
Canberk Temiz <canberk.tmz@gmail.com>
2020-12-29 18:15:09 +0100

    add :: service to centralise api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './character/character.component';
@@ -12,7 +14,9 @@ import { SearchComponent } from './shared/search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
